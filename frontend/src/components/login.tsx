0ba7b12bcf57e19be7f6f96ac8d71e0483780ec0
sital999 <sitalnagarkoti123@gmail.com>
2024-06-16 16:53:28 +0545

    Add image as profile in navbar

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -8,30 +8,35 @@ const Login = () => {
     console.log('test', session);
 
     const getFirstName = (fullName: string): string => {
-        const firstSpaceIndex = fullName?.indexOf(' ');
+        const firstSpaceIndex = fullName.indexOf(' ');
         if (firstSpaceIndex === -1) {
             return fullName;
         } else {
-            return fullName?.substring(0, firstSpaceIndex);
+            return fullName.substring(0, firstSpaceIndex);
         }
     };
-    if (session && session.user) {
-        const firstName = getFirstName(session.user.name as string);
-        return (
-            <> 
-                <Image src={session.user.image || ''} alt={session.user.name || ''} height={40} width={40} className='rounded-full'/>
-                <p className="poppins-thin text-black">Welcome, {firstName}</p>
-                &nbsp; &nbsp;
-                <span
-                    className="poppins-thin btn btn-outline btn-error text-black"
-                    onClick={() => signOut()}
-                >
-                    Sign Out
-                </span>
-            </>
-        );
-    }
-    return (
+
+    return session && session.user ? (
+        <div className="flex items-center gap-2">
+            <Image
+                src={session.user.image || ''}
+                alt={session.user.name || ''}
+                height={40}
+                width={40}
+                className="rounded-full"
+            />
+            <p className="poppins-thin text-black">
+                {getFirstName(session.user.name || '')}
+            </p>
+            &nbsp; &nbsp;
+            <span
+                className="poppins-thin btn btn-outline btn-error text-black"
+                onClick={() => signOut()}
+            >
+                Sign Out
+            </span>
+        </div>
+    ) : (
         <span
             className="poppins-thin btn btn-outline btn-success btn-wide text-black"
             onClick={() => signIn('google')}
