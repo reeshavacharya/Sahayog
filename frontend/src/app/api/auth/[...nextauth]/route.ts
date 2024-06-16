646568b502bff948f60d5e77f47432019dd80771
sital999 <sitalnagarkoti123@gmail.com>
2024-06-16 16:47:39 +0545

    Update env and add refreshToken and accessToken in session

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,47 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
+import environments from '@app/config/environments';
+import refreshAccessToken from '@app/utils/nextAuthUtils';
+
+const ClientID = environments.GOOGLE_CLIENT_ID;
+const ClientSecret = environments.GOOGLE_CLIENT_SECRET;
+const SecretKey = environments.NEXTAUTH_SECRET;
+
 const handler = NextAuth({
+    secret: SecretKey,
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+            clientId: ClientID,
+            clientSecret: ClientSecret,
+            authorization: { params: { access_type: 'offline', prompt: 'consent' } }
         })
     ],
     session: {
         strategy: 'jwt'
     },
     callbacks: {
-        async jwt({ token, account }) {
-            // Persist the OAuth access_token to the token right after signin
+        async jwt({ token, user, account, profile }) {
             if (account) {
-                token.accessToken = account.access_token;
+                return {
+                    access_token: account.access_token,
+                    expires_at: account.expires_at,
+                    refresh_token: account.refresh_token,
+                    id: token.sub,
+                    name: user?.name,
+                    email: profile?.email,
+                    image: token?.picture
+                };
+            } else if (Date.now() < (token.expires_at as number) * 1000) {
+                return token;
+            } else {
+                return refreshAccessToken(token);
             }
-            return token;
         },
         async session({ session, token }) {
+            if (token) {
+                session.user = token;
+            }
             return session;
         }
     }
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,22 +1,25 @@
 'use client';
 
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 const Login = () => {
     const { data: session } = useSession();
     console.log('test', session);
+
     const getFirstName = (fullName: string): string => {
-        const firstSpaceIndex = fullName.indexOf(' ');
+        const firstSpaceIndex = fullName?.indexOf(' ');
         if (firstSpaceIndex === -1) {
             return fullName;
         } else {
-            return fullName.substring(0, firstSpaceIndex);
+            return fullName?.substring(0, firstSpaceIndex);
         }
     };
     if (session && session.user) {
         const firstName = getFirstName(session.user.name as string);
         return (
-            <>
+            <> 
+                <Image src={session.user.image || ''} alt={session.user.name || ''} height={40} width={40} className='rounded-full'/>
                 <p className="poppins-thin text-black">Welcome, {firstName}</p>
                 &nbsp; &nbsp;
                 <span
